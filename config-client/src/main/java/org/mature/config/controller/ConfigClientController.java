5d0b8ac0c27a1ee8a0722be3c61e0801131efbbd
luyuhui0209 <luyuhui0209@163.com>
2020-01-10 14:47:05 +0800

    新增配置重新的使用案例

---
+++ config-client/src/main/java/org/mature/config/controller/ConfigClientController.java
@@ -0,0 +1,20 @@
+package org.mature.config.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope //@RefreshScope注解用于刷新配置
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+
+}
